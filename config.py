a656a214af6377630bf59888a101778740d81d38
amarjit9279586367-cyber <amarjit9279586367@gmail.com>
2026-07-30 11:15:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ import os
 
 class Config:
     # ——— Bot Settings ———
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "YOUR_BOT_TOKEN_HERE")
-    ADMIN_IDS = [int(x) for x in os.environ.get("ADMIN_IDS", "123456789").split(",")]
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8680846598:AAF8Jj4rL2m2julxKs3teSGrDtpIFXJj6tk")
+    ADMIN_IDS = [int(x) for x in os.environ.get("ADMIN_IDS", "8691519315").split(",")]
     
     # ——— Web App URL (your Render domain) ———
     # Render पर deploy करने के बाद यह अपने-आप set हो जाएगा
