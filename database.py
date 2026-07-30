5310fff34c464ae7a9e40633e1ebef954a65d76c
amarjit9279586367-cyber <amarjit9279586367@gmail.com>
2026-07-30 13:10:13 +0530

    Update database.py

--- database.py
+++ database.py
@@ -7,7 +7,8 @@ import os
 DB_PATH = "bot_data.db"
 
 def get_db():
-    conn = sqlite3.connect(DB_PATH, check_same_thread=False)
+    conn = sqlite3.connect(DB_PATH,timeout=30,
+                         check_same_thread=False)
     conn.row_factory = sqlite3.Row
     return conn
 
